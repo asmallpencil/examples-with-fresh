b4ddcedbe3b1f064c865b6e3115931e2acfde298
asmallpencil <3766344381@qq.com>
2025-09-17 13:31:33 +0800

    更新 time.ts

--- routes/api/time.ts
+++
@@ -1,9 +0,0 @@
-import { define } from "../../utils.ts";
-
-export const handler = define.handlers({
-  GET() {
-    return new Response(JSON.stringify({ now: new Date().toISOString() }), {
-      headers: { "Content-Type": "application/json" },
-    });
-  },
-});
