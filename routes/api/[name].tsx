aba323d9d211f62971b27a4653abf284d0b5cdbb
asmallpencil <3766344381@qq.com>
2025-09-17 13:30:12 +0800

    更新 [name].tsx

--- routes/api/[name].tsx
+++ routes/api/[name].tsx
@@ -1,12 +1,9 @@
 import { define } from "../../utils.ts";
 
 export const handler = define.handlers({
-  GET(ctx) {
-    const name = ctx.params.name;
-    return new Response(
-      `Hello, ${name.charAt(0).toUpperCase() + name.slice(1)}!`,
-    );
+  GET() {
+    return new Response(JSON.stringify({ now: new Date().toISOString() }), {
+      headers: { "Content-Type": "application/json" },
+    });
   },
 });
-
-
